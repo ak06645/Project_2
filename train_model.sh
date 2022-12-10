090c671a10f31ab263b02ec9dd149700a466bf7b
Bakytzhan Konratov <bakytzhan.konratov@gmail.com>
2022-12-10 17:13:34 +0100

    added training sh filee

--- train_model.sh
+++ train_model.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 #SBATCH --time=24:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
 #SBATCH --exclude=gpu04,gpu06
 #SBATCH --mem=10000
 #SBATCH --mail-type=end
-#SBATCH --mail-user=user@tu-braunschweig.de
+#SBATCH --mail-user=bahonya98@gmail.com
 
 module load anaconda/3-5.0.1
 
